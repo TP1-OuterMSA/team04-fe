7af6a9b764cfa39b679f5c072026d17cdd0fff4b
tiradovi <sunvit4@naver.com>
2025-04-22 11:33:37 +0900

    chore: 라우팅 설정 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './styles/App.css';
 import MealTableContainer from './component/MealTableContainer';
 
 const App: React.FC = () => {
-    return <MealTableContainer />;
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/team4/meal/showitems" replace />} />
+          <Route path="/team4/meal/showitems" element={<MealTableContainer />} />
+        </Routes>
+      </BrowserRouter>
+  );
 };
 
 export default App;
